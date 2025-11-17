8b8563fbf486ffb079764402adcbe16c1f8d3b74
HoshimiP <hoshimi_p@outlook.com>
2025-11-18 01:30:38 +0800

    test

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -6,5 +6,5 @@ use axstd::println;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("\x1B[32m[WithColor]: Hello, Arceos!\x1B[0m");
+    println!("[WithColor]: Hello, Arceos!");
 }
